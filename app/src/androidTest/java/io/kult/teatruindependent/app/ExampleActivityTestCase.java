7daf502987cdd26d2042acf9003142b7c7f368fa
Cargo Kult <cargokultist@gmail.com>
2014-04-27 16:27:40 +0300

    added test capabilities, festassert support, example tests

---
+++ app/src/androidTest/java/io/kult/teatruindependent/app/ExampleActivityTestCase.java
@@ -0,0 +1,10 @@
+package io.kult.teatruindependent.app;
+
+import android.test.ActivityTestCase;
+
+public class ExampleActivityTestCase extends ActivityTestCase {
+
+    public void testActivityTestCase() {
+        assertEquals(true, true);
+    }
+}
---
+++ app/src/androidTest/java/io/kult/teatruindependent/app/ExampleAndroidTestCase.java
@@ -0,0 +1,12 @@
+package io.kult.teatruindependent.app;
+
+import android.test.AndroidTestCase;
+
+import org.fest.assertions.api.Assertions;
+
+public class ExampleAndroidTestCase extends AndroidTestCase {
+
+    public void testAndroidTestCase() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
